7e992fb77e24fbb91f549d4669d7ba243e16e1eb
Wojciech Kajewski <150830579+Kajo92@users.noreply.github.com>
2024-03-30 04:03:04 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,61 @@
+const products = [
+    {
+        "id": 1,
+        "name":" Tornado Flame",
+        "price": 989,
+        "image": "images/1.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 2,
+        "name":" Bright Flame",
+        "price": 1490,
+        "image": "images/2.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 3,
+        "name":" Daszek TR",
+        "price": 50,
+        "image": "images/3.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 4,
+        "name":" Palnik Gazowy",
+        "price": 25,
+        "image": "images/4.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 5,
+        "name":" Magiczny płomień",
+        "price": 9,
+        "image": "images/5.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 6,
+        "name":" Pellet 5 kg",
+        "price": 19,
+        "image": "images/6.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 7,
+        "name":" Pellet 10 kg",
+        "price": 34,
+        "image": "images/7.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    },
+    {
+        "id": 8,
+        "name":" Pellet 15 kg",
+        "price": 43,
+        "image": "images/8.png",
+        "description": "PRODUKT NIEDOSTĘPNY"
+    }
+
+];
+export default products;
+
